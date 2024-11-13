a8e13a7096e7b6d9a1efd53094db89f529e1e733
xazhao <xazhao@126.com>
2024-11-13 16:39:03 +0800

    🎉 Java并发编程

---
+++ work-concurrent-program/src/main/java/com/xazhao/concurrent/WorkConcurrentProgramApplication.java
@@ -0,0 +1,23 @@
+package com.xazhao.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description Created on 2024/11/13.
+ * @Author Zhao.An
+ */
+
+@Slf4j
+@MapperScan(basePackages = "com.xazhao.**.mapper")
+@SpringBootApplication
+public class WorkConcurrentProgramApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(WorkConcurrentProgramApplication.class, args);
+        log.info("(♥◠‿◠)ﾉﾞ  work-concurrent-program启动成功...   ლ(´ڡ`ლ)ﾞ");
+    }
+}
